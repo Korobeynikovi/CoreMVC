2e1c9f4ae4b8372fa9653944f57b0c756facbcc3
Илья Коробейников <Korobeynikovi@yandex.ru>
2021-06-28 10:14:27 +0700

    add

---
+++ CoreMVC/Domain/DataManager.cs
@@ -0,0 +1,16 @@
+﻿using CoreMVC.Domain.Repositories.Abstract;
+
+namespace CoreMVC.Domain
+{
+    public class DataManager
+    {
+        public ITextFieldsRepository TextFields { get; set; }
+        public IServiceItemsRepository ServiceItems { get; set; }
+
+        public DataManager(ITextFieldsRepository textFieldsRepository, IServiceItemsRepository serviceItemsRepository)
+        {
+            TextFields = textFieldsRepository;
+            ServiceItems = serviceItemsRepository;
+        }
+    }
+}
