6a049d9e9f451b37bffe1237399152d171434a40
Korobeynikovi <korobeynikov.dev@gmail.com>
2022-04-01 16:23:56 +0700

    correct main

--- CoreMVC/Program.cs
+++ CoreMVC/Program.cs
@@ -11,10 +11,9 @@ namespace CoreMVC
 {
 	public class Program
 	{
-		public static void Main(string[] args)
-		{
+		public static void Main(string[] args) =>
 			CreateHostBuilder(args).Build().Run();
-		}
+		
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
